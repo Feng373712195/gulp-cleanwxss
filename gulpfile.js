995abdcb3759d97127b8d74bdcdff804c984d6c9
Feng373712195 <373712195@qq.com>
2019-06-12 23:53:01 +0800

    后台选择器完成

--- gulpfile.js
+++ gulpfile.js
@@ -364,13 +364,6 @@ gulp.task('one',async function(){
 
         //选择器只匹配一个元素
         if( selectNodes.length == 1 ){
-            // if( ~selectNodes[0].indexOf('>') ){
-            //    return checkChildSelectQuery(selectNodes[0]) 
-            // }
-            // if( ~selectNodes[0].indexOf('+') ){
-            //     return checkAdjacentSelectQuery(selectNodes[0]) 
-            // }
-            // that.select = _checkHasSelect(selectNodes[0]) ? true : false
             return  _checkHasSelect(selectNodes[0]) ? true : false
         }
         //多元素选择器
@@ -384,7 +377,7 @@ gulp.task('one',async function(){
                     const selectQueryHandles = {
                         '>':checkChildSelectQuery,
                         '+':checkAdjacentSelectQuery,
-                        '~':function(){}
+                        '~':checkProgenySelectQuery
                     }
                     let selectQueryType = selectNodes[i2][0]
                     let classSelect = selectNodes[i2].slice(1);
@@ -490,6 +483,36 @@ gulp.task('one',async function(){
         return newFinds; 
     }    
 
+    // 检查element1~element2选择器是否生效
+    const checkProgenySelectQuery = (classSelect,findNodes = null) => {
+        const newFinds = [];
+        if( findNodes ){
+            const progenySelectType = classSelect[0] == '#' ? 'id' : classSelect[0] == '.' ? 'class' : 'tag';
+            findNodes.nodes.forEach((node,index)=>{
+                // 获取父级内的所有同级元素
+                const brothers = Object.values( node.parent.obj.childs ).map((n,key)=> Object.values(n)[0] )
+                //  找到自己在同级元素中的开始标签索引位置
+                const selfIndex = brothers.indexOf( node )
+                // 寻找此标签闭合后下一个的索引
+                // let otherBrotherNodeStartIndex = findCloseTagIndex(brothers,node)
+                // 得到闭合标签后的所有元素
+                const otherBrotherNode = brothers.slice( 0,selfIndex );
+                // 寻找 相领选择器 对应元素
+                if( otherBrotherNode.length > 0 ){
+                    otherBrotherNode.reverse().forEach(node=>{
+                        const progenyNodeSelect = progenySelectType != 'tag' ? classSelect.slice(1) : classSelect;
+                        if( progenySelectType == 'id' && node.id == progenyNodeSelect ) newFinds.push(node)
+                        else if( progenySelectType == 'class' && ~node.class.indexOf( progenyNodeSelect ) ) newFinds.push(node)
+                        else if( progenySelectType == 'tag' && node.tag == secondSelect ) newFinds.push(node)
+                    })
+                }
+            })
+        }else{
+            console.err('checkProgenySelectQuery not findNodes')
+        }
+        return newFinds; 
+    }
+
     // 检查兄弟选择器是否生效
     const checkChildSelectQuery = (classSelects,findNodes = null) => {
         const newFinds = [];
@@ -505,6 +528,8 @@ gulp.task('one',async function(){
             return finds
         }
     }
+
+
 
     //从子节点开始查找
     for( let i = 0 ,len = classSelects.length; i < len; i++ ){
