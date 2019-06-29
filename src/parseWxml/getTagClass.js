1317b240255ec8d09e3102cd39adaf3fee2121ab
Feng373712195 <373712195@qq.com>
2019-06-29 13:03:12 +0800

    改造getTagClass完成

--- src/parseWxml/getTagClass.js
+++ src/parseWxml/getTagClass.js
@@ -4,9 +4,15 @@ const getTernaryExpressionClass = require('./getTernaryExpressionClass');
 // 是否为三元表达式
 const ternaryExpressionReg = /(.*?)\?(.*):(.*)/;
 
+// 是否为模版渲染class
 const isDynamicReg = /{{.*}}/g;
+
+// 是否为拼接class
+const joinLeftClassReg = /([-_\w]+)\{\{(.*?)\}\}/;
+const joinRightClassReg = /\{\{(.*?)\}\}([-_\w]+)/;
+
 // 是否字符串正则表达式
-// const isStringReg = /['|"](.*?)['|"]/;
+const isStringReg = /['|"](.*?)['|"]/;
 
 // 取得标签内的Class
 // 注意还有hover-class 之类的情况
@@ -25,43 +31,62 @@ function getTagClass(classKey, tag, cssVariable, arr) {
     // 获得结束位置
     const endIndex = tag.substring(startIndex + classKey.length + 2, tag.length).indexOf(startMark);
     // 取得整段class
-    let TagClassStr = tag.substring(startIndex, startIndex + endIndex + classKey.length + 3);
+    const TagClassStr = tag.substring(startIndex, startIndex + endIndex + classKey.length + 3);
 
     const classContentReg = new RegExp(`${classKey}\\=\\${startMark}(.*)\\${startMark}`);
     let classContent = classContentReg.exec(TagClassStr)[1];
     classContent = classContent.replace(isDynamicReg, $1 => $1.replace(/\s/g, ''));
-    console.log(classContent, 'classContent');
     const classContentSlice = classContent.split(' ');
-    console.log(classContentSlice);
-
-    // 获取动态选人的class
-    const dynamicClassReg = /([-_\w]+)?\{\{(.*?)\}\}([-_\w]+)?/;
-
-    let dynamicClass = '';
-
-    while (dynamicClass = dynamicClassReg.exec(TagClassStr)) {
-      if (ternaryExpressionReg.test(dynamicClass[2])) {
-        let res = getTernaryExpressionClass(dynamicClass[2], cssVariable);
-        res = res.map(className => `${dynamicClass[1] ? dynamicClass[1] : ''}${className}${dynamicClass[3] ? dynamicClass[3] : ''}`);
-        TagClass = TagClass.concat(res);
-      } else if (dynamicClass[2] != '') {
-        dynamicClass[2] = dynamicClass[2].trimLeft().trimRight();
-        let res = getDynamicClass(dynamicClass[2], cssVariable);
-        res = res.map(className => `${dynamicClass[1] ? dynamicClass[1] : ''}${className}${dynamicClass[3] ? dynamicClass[3] : ''}`);
-        TagClass = TagClass.concat(res);
-        // if (!isStringReg.test(dynamicClass[2])) {
-        // _cssVariable.add(dynamicClass[2]);
-        // }
+    classContentSlice.forEach((slice) => {
+      if (isDynamicReg.test(slice)) {
+        if (joinLeftClassReg.test(slice) || joinRightClassReg.test(slice)) {
+          slice = slice.replace(/(\{\{|\}\})/g, $1 => ($1[0] === '{' ? ` ${$1}` : `${$1} `));
+          slice = slice.split(' ')
+            .map(item => (isDynamicReg.test(item) ? item.replace(/(\{\{|\}\})/g, '').replace(isStringReg, '$1') : `"${item}"`))
+            .join(' + ');
+          const res = getDynamicClass(slice, cssVariable);
+          TagClass = TagClass.concat(res);
+        } else if (ternaryExpressionReg.test(slice)) {
+          const res = getTernaryExpressionClass(slice, cssVariable);
+          TagClass = TagClass.concat(res);
+        } else {
+          const res = getDynamicClass(slice, cssVariable);
+          TagClass = TagClass.concat(res);
+        }
+      } else if (slice) {
+        TagClass.push(slice);
       }
-      TagClassStr = TagClassStr.replace(dynamicClass[0], '');
-    }
-
-    TagClassStr.replace(new RegExp(`${classKey}\\=[\\'|\\"](.*)[\\'|\\"]`), (classStr, classNames) => {
-      TagClass = TagClass.concat(classNames.split(' ').filter(v => v));
     });
 
+    console.log(TagClass, 'TagClass');
+
+
+    // OLD
+    // 获取动态选人的class
+    // const dynamicClassReg = /([-_\w]+)?\{\{(.*?)\}\}([-_\w]+)?/;
+    // const dynamicClass = '';
+    // while (dynamicClass = dynamicClassReg.exec(TagClassStr)) {
+    // if (ternaryExpressionReg.test(dynamicClass[2])) {
+    //   let res = getTernaryExpressionClass(dynamicClass[2], cssVariable);
+    //   res = res.map(className => `${dynamicClass[1] ? dynamicClass[1] : ''}${className}${dynamicClass[3] ? dynamicClass[3] : ''}`);
+    //   TagClass = TagClass.concat(res);
+    // } else if (dynamicClass[2] != '') {
+    //   dynamicClass[2] = dynamicClass[2].trimLeft().trimRight();
+    //   let res = getDynamicClass(dynamicClass[2], cssVariable);
+    //   res = res.map(className => `${dynamicClass[1] ? dynamicClass[1] : ''}${className}${dynamicClass[3] ? dynamicClass[3] : ''}`);
+    //   TagClass = TagClass.concat(res);
+    // if (!isStringReg.test(dynamicClass[2])) {
+    // _cssVariable.add(dynamicClass[2]);
+    // }
+    // }
+    // TagClassStr = TagClassStr.replace(dynamicClass[0], '');
+    // }
+    // TagClassStr.replace(new RegExp(`${classKey}\\=[\\'|\\"](.*)[\\'|\\"]`), (classStr, classNames) => {
+    //   TagClass = TagClass.concat(classNames.split(' ').filter(v => v));
+    // });
+
     // 一些写法不规范的开发者 会写多个class 这里先不管
-    tag = tag.replace(new RegExp(`(${classKey}\\=[\\'|\\"].*?[\\'|\\"])`), '');
+    tag = tag.replace(classContentReg, '');
     if (hasClass.test(tag)) {
       return getTagClass(classKey, tag, cssVariable, TagClass);
     }
